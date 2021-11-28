820f3b9eca85decb4d6646258c0e1ec92eb204b1
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-11-28 16:50:20 +0200

    Add API-Service
    Adds 3 functions to POST, DELETE, GET contacts from backend

---
+++ src/services/mockaApi.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://61a2a5ef014e1900176de9af.mockapi.io/api/v1/';
+const endpoint = 'contacts';
+
+axios.defaults.baseURL = BASE_URL;
+
+export async function fetchContacts() {
+  try {
+    const { data } = await axios.get(`${endpoint}`);
+    return data;
+  } catch (error) {
+    console.log('Houston, we have a problem: ', error.message);
+    return error;
+  }
+}
+
+export async function addContact(contact) {
+  try {
+    const { data } = await axios.post(`${endpoint}`, contact);
+    return data;
+  } catch (error) {
+    console.log('Houston, we have a problem: ', error.message);
+    return error;
+  }
+}
+
+export async function deleteContact(id) {
+  try {
+    const data = await axios.delete(`${endpoint}/${id}`);
+    return data;
+  } catch (error) {
+    console.log('Houston, we have a problem: ', error.message);
+    return error;
+  }
+}
